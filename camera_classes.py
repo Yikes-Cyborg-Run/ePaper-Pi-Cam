af8ec35e614aee4209ab775bf2daea63e1b87e79
Yikes-Cyborg-Run <40796089+Yikes-Cyborg-Run@users.noreply.github.com>
2025-05-19 06:29:58 -0400

    Add files via upload

--- camera_classes.py
+++ camera_classes.py
@@ -1,65 +1,64 @@
-import time, datetime, os, logging, sys, keyboard
+import time, datetime, os, logging, re, keyboard
 from gpiozero import LED, Button
 from picamzero import Camera
 from waveshare_epd import epd2in7_V2 # -- the 2.7inch GPIO HAT
 from PIL import Image, ImageDraw, ImageFont
 
 class Menu():
-
-    def build(h, selection, menu_list):
+    def build(h, epd, selection, menu_list, photo_list):
+        config=Config.load()
+        font_path="/home/pi/ePaper-Pi-Cam/Fonts/"+config["font"]
+        fontsize=int(config["fontsize"])
         use_menu=menu_list[selection]
-        if selection=="Main Menu":
-            go_back_to="Main Menu"
-        if selection=="Camera Options":
-            go_back_to="Main Menu"
-
-        print(str(selection).upper())
+        image=Image.new("1", (epd.height, epd.width), 255)
+        draw=ImageDraw.Draw(image)
+        font=ImageFont.truetype(font_path, fontsize+4)
+        draw.text((20,10),selection.upper(),font=font,fill=0)
+        font=ImageFont.truetype(font_path, fontsize)
+        Log.log(f"{selection.upper()} -- {use_menu}" ,1)
+        y=40
         highlighted=use_menu[h]
         for item in use_menu:
+            item=str(item)
+            if item=="Autoscroll" or item =="Manual Scroll":
+                show=item+f" - {len(photo_list)} photos"
+            else:
+                show=item
             if item==highlighted:
-                item=">"+item
-            print (f"item: {item}")
+                show="-- "+show
+            draw.text((20,y),show,font=font,fill=0)
+            y+=20
+        epd.display(epd.getbuffer(image))
         return
 
-    def select(h, selection, menu_list, ignore_list):
+    def select(h, selection, menu_list, ignore_list, p, m, u, d):
         use_menu=menu_list[selection]
-        final_data=[h,selection]
+        final_data=[h,selection,True]
         limit=len(use_menu)-1
-        
+
         if selection not in ignore_list:
-            k=keyboard.read_key()
-            time.sleep(0.2)
-            print(f"pressed: {k}")
-            if k:
-                if k=="up":
-                    h-=1 
-                    if h<0:h=limit
-                    final_data=[h, selection]
-                if k=="down":
-                    h+=1
-                    if h>limit:h=0
-                    final_data=[h, selection]
-                if k=="enter":
-                    final_data=[0, use_menu[h]]
-                    if selection in menu_list["Camera Options"]:
-                        option=selection.lower()
-                        option.replace(" ", "_")
-                        option.replace("-", "")
-                        final_data=Config.save(option, use_menu[h])
-                if k=="esc":
-                    final_data=[0,"Main Menu"]
-
+            if p.is_pressed:
+                final_data=[0, use_menu[h],False]
+                if selection in menu_list["Camera Options"]:
+                    final_data=Config.save(selection, use_menu[h])
+            elif m.is_pressed:
+                final_data=[0,"Main Menu", False]
+            elif u.is_pressed:
+                h+=1
+                if h>limit:h=0
+                final_data=[h, selection,False]
+            elif d.is_pressed:
+                h-=1 
+                if h<0:h=limit
+                final_data=[h, selection,False]
         else:
-            final_data=[0,selection]
-
+            final_data=[0,selection,True]
         return final_data
 
 class Config():
-    def __init__(self):
-        pass
-
     def load():
-        config_path=r"C:/Users/ckingsbury/OneDrive - City of Port Orange/Desktop/ePaper-Pi-Cam/config.txt"
+        Log.log(f"Loading config....",1)
+        config_path=r"/home/pi/ePaper-Pi-Cam/config.txt"
         config={}
         if os.path.exists(config_path):
             with open(config_path, 'r') as file:
@@ -67,116 +66,161 @@ class Config():
                     if "=" in line:
                         key, value=line.strip().split("=", 1)
                         config[key]=value
-                        print(f" loaded: {key} -- {value}")
-            print("Loaded config.")
+                        #Log.log(f"Loaded: {key} -- {value}",1)
+            Log.log("Loaded config.",1)
         else:
-            print("No config file.")
+            Log.log("No config file.",1)
         return config
 
-    def save(passed_key, passed_value):
+    def save(k, v):
+        epd=epd2in7_V2.EPD()
+        epd.init()
+        k=k.lower()
+        k=re.sub(r'[^a-zA-Z0-9]', '', k)
         config=Config.load()
-        config[passed_key]=passed_value
-        config_path=r"C:/Users/ckingsbury/OneDrive - City of Port Orange/Desktop/ePaper-Pi-Cam/config.txt"
-        print(f"Saving options....\n{passed_key} - {passed_value}")
+        config[k]=str(v)
+        config_path=r"/home/pi/ePaper-Pi-Cam/config.txt"
+        Log.log(f"Saving options....\n{k} - {v}",1)
         # Save updated config
         with open(config_path, 'w') as file:
             for key, value in config.items():
                 file.write(f"{key}={value}\n")
-            print("Saved!")
             time.sleep(.5)
-        print("Options saved")
-        return [0,"Camera Options"]
+        Log.log("Saved config",1)
+        return [0,"Camera Options",False]
 
 class Warn():
-    def warn(selection):
-        if selection=="Camera":
-            print("Camera Message")
-            selection="Take Photos"
+    def warn(epd, selection, photo_list, num):
+        config=Config.load()
+        font_path="/home/pi/ePaper-Pi-Cam/Fonts/"+config["font"]
+        fontsize=int(config["fontsize"])
 
-        elif selection=="Delete Single Photo":
-            print("Delete Warning")
-            selection = "Purge Confirmed"
-
-        elif selection=="Delete All Photos":
-            print("Delete Warning")
-            selection="Purge Confirmed"
-
-        # !!!!!!! KEY PRESS HERE TO SEND THE ACTION
-        return [0, selection]
-
-    def no_photos():
-        global head_fs, base_fs
-        log("List selected, but no photos on file.", 0)
-        LEDs(0,0,1)
         image=Image.new("1", (epd.height, epd.width), 255)
         draw=ImageDraw.Draw(image)
-        font=ImageFont.truetype(font_path, head_fs)
+        font=ImageFont.truetype(font_path, fontsize+4)
+        draw.text((20,10),selection.upper(),font=font,fill=0)
+
+        if selection=="Camera":
+            draw.text((20,50),"Camera Ready \nPress photo button.",font=font,fill=0)
+            selection="Take Photos"
+
+        elif selection=="Delete All Photos":
+            draw.text((20,50),"No photos to show.",font=font,fill=0)
+            print("Purge ALL Warning")
+  #          selection="Purge Confirmed"
+
+        elif selection=="Delete Single Photo":
+            Log.log("Delete single photo warning...", 1)
+            filename=photo_list[num]
+            Log.log(f"Check delete: {filename}",1)
+            image=Image.open(filename)
+            image=image.resize((epd.height, epd.width))
+ #           image=image.resize((50, 100))
+            draw=ImageDraw.Draw(image)
+            font=ImageFont.truetype(font_path, fontsize+4)
+            draw.text((20,10), "Are you SURE you want to \ndelete this photo?", font=font, fill=0)
+            font=ImageFont.truetype(font_path, fontsize)
+            draw.text((20,10), "Press Menu button to cancel\nPress Photo button to delete", font=font, fill=0)
+
+        epd.display(epd.getbuffer(image))
+        # !!!!!!! KEY PRESS HERE TO SEND THE ACTION
+        return [0, selection, True]
+
+    def no_photos(epd):
+        Log.log("List selected, but no photos on file.", 0)
+        LEDs.LEDs(0,0,1)
+        config=Config.load()
+        font_path="/home/pi/ePaper-Pi-Cam/Fonts/"+config["font"]
+        fontsize=int(config["fontsize"])
+        image=Image.new("1", (epd.height, epd.width), 255)
+        draw=ImageDraw.Draw(image)
+        font=ImageFont.truetype(font_path, fontsize+4)
         draw.text((20,50),"No photos to show.",font=font,fill=0)
-        font=ImageFont.truetype(font_path, base_fs)
+        font=ImageFont.truetype(font_path, fontsize)
         draw.text((20,100),"Press menu button.",font=font,fill=0)
         epd.display(epd.getbuffer(image))
-            
-
-
-        k=keyboard.read_key()
-        if k:
-            if k=="enter":
-                return [0, selection]
-            elif k=="esc":
-                return [0, "Main Menu"]
 
 class Action():
     def act(selection):
-        print(f"Action: {selection}")
+        Log.log(f"Action: {selection}",0)
 
-    def take_photo(photo_list, image_folder, timestamp_photo):
-        LEDs.LEDs(0,0,1)
-        timestamp=datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # Get the current timestamp
-        filename=f"{timestamp}.jpg" # Construct the filename
-        if timestamp_photo==True: # Check if timestamping is enabled in config...
-            cam.annotate(timestamp, 'plain-small', 'white', 1, 2, [5,170]) # add a timestamp to photo if it is
-        cam.take_photo(image_folder+filename)
-        img_path=os.path.join(image_folder, filename)
-        image=Image.open(img_path)
-        image=image.resize((epd.height, epd.width))
-        epd.display(epd.getbuffer(image)) # Display the final image
-        LEDs.LEDs(1,0,0)
-        photo_list.append(img_path)
-        return [0,"Take Photos",photo_list]
+    def take_photo(p, cam, epd, photo_list, image_folder):
+        if p.is_pressed:
+            LEDs.LEDs(0,0,1)
+            config=Config.load()
+            timestamp_photo=config["timestampphoto"]
+            timestamp=datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") # Get the current timestamp
+            filename=f"{timestamp}.jpg" # Construct the filename
+            if timestamp_photo==True: # Check if timestamping is enabled in config...
+                cam.annotate(timestamp, 'plain-small', 'white', 1, 2, [5,170]) # add a timestamp to photo if it is
+            cam.take_photo(image_folder+filename)
+#            epd=epd2in7_V2.EPD()
+#            epd.init()
+            img_path=os.path.join(image_folder, filename)
+            image=Image.open(img_path)
+            image=image.resize((epd.height, epd.width))
+            epd.display(epd.getbuffer(image)) # Display the final image
+            LEDs.LEDs(1,0,0)
+            photo_list.append(img_path)
+        return photo_list
 
-    def display_photo(photo_list, key):
+    def display_photo(epd, photo_list, key):
         global head_fs, base_fs
-        log("Loading file...", 1)
+        Log.log("Loading file...", 1)
         filename=photo_list[key]
         image=Image.open(filename)
         image=image.resize((epd.height, epd.width))
         epd.display(epd.getbuffer(image))
-        log("Displayed file: "+filename,1)
+        Log.log("Displayed file: "+filename,1)
         return None
     
-    def manual_scroll(photo_list, photo_increment):
-        if(len(photo_list)>0):
-            k=keyboard.key_pressed()
+    def manual_scroll(u, d, epd, photo_list, data):
+        num=data[0]
+        drawn=data[1]
+        if len(photo_list)>0:
             limit=len(photo_list)-1
-            if k:
-                if k=="up":
-                    photo_increment+=1
-                    if(photo_increment>=limit):photo_increment=0
-                if k=='down':
-                    photo_increment+=1
-                    if(photo_increment>=limit):photo_increment=0
-                Action.display_photo(photo_increment)
+            if drawn==True:
+                if u.is_pressed:
+                    num+=1
+                    if num>limit:num=0
+                    return [num, False]
+                elif d.is_pressed:
+                    print("d")
+                    num-=1
+                    if num<0: num=limit
+                    return [num, False]
+                return [num, True]
+            else:
+                Action.display_photo(epd, photo_list, num)
+                return[num, True]
         else:
-            Warning.no_photos()
-        return photo_increment
-    
+            Warn.no_photos(epd)
 
-    
-
-
+    def autoscroll(epd, photo_list, autoscroll_duration):
+        if len(photo_list)>0:
+            if drawn==True:
+                now_time=datetime.datetime.now()
+                now=int(time.mktime(now_time.timetuple()))
+                if now>future:
+                    list_increment+=1
+                    if list_increment>=len(photo_list)-1:
+                        list_increment=0
+                    Log.log("Autoscroll Increment: "+str(list_increment), 1)
+                    display_photo(epd, photo_list, list_increment)
+                    Calc.future(autoscroll_duration)
+            else:
+                Action.display_photo(epd, photo_list, list_increment)
+                Calc.future(autoscroll_duration)
+        else:
+            if drawn==False:
+                drawn=True
+                no_photos_msg()
 
 class LEDs():
-    def LEDs(green,yellow,red):
+    def LEDs(green, yellow, red):
+        LED_G=LED(20)
+        LED_Y=LED(16)
+        LED_R=LED(12)
         if green==1: LED_G.on()
         else:  LED_G.off()
         if yellow==1: LED_Y.on()
@@ -185,8 +229,7 @@ class LEDs():
         else: LED_R.off()
 
 class Calc():
-    def future():
-        global now_time, now, future_time, future, drawn, autoscroll_duration
+    def future(autoscroll_duration):
         # Get the current timestamp
         now_time=datetime.datetime.now()
         now=int(time.mktime(now_time.timetuple()))
@@ -196,8 +239,7 @@ class Calc():
         # Print the original and updated timestamps
         Log.log("   NOW: "+str(now),1)
         Log.log("FUTURE: "+str(future),1)
-        drawn=True
-        return None
+        return future
 
     def timelapse_text():
         config=Config.load()
@@ -217,3 +259,16 @@ class Calc():
         else:
             final=suffix
         return final
+
+class Log():
+    def log(msg, err_type):
+        log_path="/home/pi/ePaper-Pi-Cam/log.log"
+        logger=logging.getLogger(log_path)
+        logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%I:%M:%S %p',)
+        err=["ERROR", "INFO", "DEBUG", "CRITICAL", "WARNING"]
+        print(err[err_type]+" : "+msg)
+        if(err_type==0):logger.error(msg)
+        elif(err_type==1):logger.info(msg)
+        elif(err_type==2):logger.debug(msg)
+        elif(err_type==3):logger.critical(msg)
+        elif(err_type==4):logger.warning(msg)
--- main.py
+++ main.py
@@ -1,67 +1,113 @@
-from camera_classes import Config, Warn, Action, Menu
-import os, time
+from camera_classes import Config, Warn, Action, Menu, Log
+import time, datetime, os, sys
+from waveshare_epd import epd2in7_V2 # -- the 2.7inch GPIO HAT
+from gpiozero import LED, Button
+from picamzero import Camera
+from waveshare_epd import epd2in7_V2 # -- the 2.7inch GPIO HAT
+from PIL import Image, ImageDraw, ImageFont
 
 def main():
-	data=[0, "Main Menu"]
-	photo_increment=0
-	photo_list=[]
+	now=datetime.datetime.now()
+	todays_date=now.strftime("%Y-%M-%D")
+	with open("/home/pi/ePaper-Pi-Cam/log.log", "w") as file:
+		file.write('####### '+todays_date+' #######\n')
+		file.close()
+
+	# Initialize the display
+	epd=epd2in7_V2.EPD()
+	epd.init()
+
 	image_folder="/home/pi/ePaper-Pi-Cam/photos/"
-#	font_directory="/home/pi/ePaper-Pi-Cam/Fonts"
-	font_directory=r"C:/Users/ckingsbury/OneDrive - City of Port Orange/Desktop/ePaper-Pi-Cam/Fonts"
+	font_directory="/home/pi/ePaper-Pi-Cam/Fonts"
 	font_list=[]
 	for f in os.listdir(font_directory):
 		if os.path.isfile:
 			os.path.join(font_directory, f)
 			font_list.append(f)
 
-	ignore_list=["Take Photo", "Time-lapse Camera", "Autoscroll", "Manual Scroll", "Delete All Photos", "Delete All Photos", "Delete Single", "Camera", "Time-Lapse Camera"]
 	menu_list={
-                "Main Menu":["Camera", "Camera Options", "Time-lapse Camera", "Autoscroll", "Manual Scroll", "Delete All Photos"],
-                "Camera Options":["Font Selection", "Font Size", "Display Rotation", "Autoscroll Duration", "Time-Lapse Duration", "White Balance", "Shut Down", "Delete Photos"],
+                "Main Menu":["Manual Scroll", "Camera", "Camera Options", "Time-lapse Camera", "Autoscroll", "Delete All Photos"],
+                "Camera Options":["Font", "Font Size", "Display Rotation", "Autoscroll Duration", "Time-Lapse Duration", "White Balance", "Shut Down", "Delete Photos"],
                 "White Balance":["auto", "tungsten", "fluorescent", "indoor", "daylight", "cloudy"],
-                "Font Size":["-4", "-2", "0", "+2", "+4"],
-                "Autoscroll Duration":[10, 30, 60, 120, 300, 600],
-                "Time-lapse Duration":[1, 30, 60, 300, 600, 1800, 3600],
-                "Display Rotation":[90, 180, 270],
+                "Font Size":["14","18","20","22"],
+                "Autoscroll Duration":["10", "30", "60", "120", "300", "600"],
+                "Time-lapse Duration":["1", "30", "60", "300", "600", "1800", "3600"],
+                "Display Rotation":["90", "180", "270"],
                 "Timestamp Photo":["True", "False"],
-                "Font Selection": font_list
-    #			"Font Selection":[f for f in os.listdir(font_directory) if os.path.isfile(os.path.join(font_directory, f))]
+                "Font": font_list,
                 }
-	action_list=["Take Photo", "Time-lapse Camera", "Autoscroll", "Manual Scroll", "Delete All Photos"]
-	warn_list=["Delete All Photos", "Delete Single", "Camera", "Time-Lapse Camera"]
-	config=Config.load()
+	ignore_list=["Take Photo", "Time-lapse Camera", "Autoscroll", "Manual Scroll", "Delete All Photos", "Delete All Photos", "Delete Single", "Camera", "Time-Lapse Camera"]
+	action_list=["Take Photos", "Time-lapse Camera", "Autoscroll", "Manual Scroll", "Delete All Photos"]
+	warn_list=["Camera", "Delete All Photos", "Delete Single Photo", "Camera", "Time-Lapse Camera"]
 
-	# Build main menu before loop begins
-#	Menu.build(0, "Main Menu", menu_list)
-
+	"""
 	# Show startup on screen
 	splash_photo=["/home/pi/ePaper-Pi-Cam/Resources/splash.jpg"]
-	Action.display_photo(splash_photo, 0)
+	Action.display_photo(epd, splash_photo, 0)
 	image=Image.new("1", (epd.height, epd.width), 255)
 	draw=ImageDraw.Draw(image)
-	font=ImageFont.truetype(font_path, head_fs)
+	font=ImageFont.truetype(font_path, fontsize+4)
 	draw.text((20,50),"Starting up...",font=font,fill=0)
 	epd.display(epd.getbuffer(image))
-	time.sleep(2)
+	"""
+
+	config=Config.load()
+	font_path="/home/pi/ePaper-Pi-Cam/Fonts/"+config["font"]
+	fontsize=int(config["fontsize"])
+	home_dir=os.environ['HOME'] # set home dir
+	print(home_dir)
+	cam=Camera() # Start camera
+	cam.greyscale=True # Take photos in black & white
+	cam.still_size=(264, 176) # resolution of the 2.7 GPIO display
+	cam.brightness=int(config["brightness"]) # can be -1.0 - 1.0
+	cam.whitebalance=str(config["whitebalance"]) # see variable above for options
+	image_folder="/home/pi/ePaper-Pi-Cam/photos/" # Where photos will be saved
+
+	# Build a list of saved photos already on file. 
+	# New photos will appended to the end of list.
+	Log.log("Building list of previously saved photos", 1)
+	photo_list=[]
+	for filename in os.listdir(image_folder):
+		if filename.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif")):
+			img_path=str(os.path.join(image_folder, filename))
+			photo_list.append(img_path)
+	Log.log(f"Photos currently on file: {len(photo_list)}.",1)
+
+	p=Button(5, pull_up=True) # used to take photo or select a menu item
+	m=Button(19, pull_up=True) # opens the menu/goes back
+	u=Button(13, pull_up=True) # menu selection +1
+	d=Button(6, pull_up=True) # menu selection -1
+
+	data=[0, "Main Menu", False]
+	scroll_data=[0, False]
 
 	while True:
 		h=data[0]
 		selection=data[1]
-#		print(f"{selection} - h: {h} ")
-		if selection in warn_list:
-			print("warn")
-			data=Warn.warn(selection)
+		drawn=data[2]
+
+		if m.is_pressed:
+			data=[0,"Main Menu", False]
+			scroll_data=[0, False]
+		elif selection in warn_list:
+			Log.log("-- Warning...", 1)
+			data=Warn.warn(epd, selection, photo_list, scroll_data[0])
 		elif selection in action_list:
-			print("its an action")
-			if selection =="Take Photo":
-				Action.take_photo(config, photo_list, image_folder)
-#				data=[0, selection]
-			if selection =="Manual Scroll":
-				photo_increment=Action.manual_scroll(photo_list, photo_increment)
-#				data=[0, selection]
+			if selection=="Take Photos":
+				photo_list=Action.take_photo(p, cam, epd, photo_list, image_folder)
+
+			elif selection=="Manual Scroll":
+				scroll_data=Action.manual_scroll(u, d, epd, photo_list, scroll_data)
+				if p.is_pressed:
+					data=[0,"Delete Single Photo",False]
+
+			elif selection=="Autocroll":
+				scroll_data=Action.autoscroll(epd, photo_list, scroll_data)
 		else:
-			Menu.build(h, selection, menu_list)
-			data=Menu.select(h, selection, menu_list, ignore_list)
+			if drawn==False:
+				Menu.build(h, epd, selection, menu_list, photo_list)
+				drawn=True
+			data=Menu.select(h, selection, menu_list, ignore_list, p, m, u, d)
 
 if __name__ == "__main__":
     main()

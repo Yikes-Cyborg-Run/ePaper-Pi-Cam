f1bbf519dfdb1c473b9c5a5be8d034ac81ebd60c
Yikes-Cyborg-Run <manager@theparsley.com>
2025-06-16 15:00:46 -0400

    edit tomli

--- tomli_test.py
+++ tomli_test.py
@@ -1,42 +1,36 @@
-import tomli
+# https://tomlkit.readthedocs.io/en/latest/
+
+import tomlkit, re
 from pathlib import Path
 
+class Config():
+	def __init__(self):
+		self.home_dir=Path(__file__).parent.resolve()
+#		self.config_path=self.home_dir / 'config.txt'
+		self.config_path=self.home_dir / 'config.toml'
 
-def load_config(config_path):
-    try:
-        with open(config_path, "rb") as f:
-            data = tomli.load(f)
-        return(data)
-    except FileNotFoundError:
-        print(f"Error: File not found at '{file_path}'")
-        return None
-    except tomli.TOMLDecodeError as e:
-        print(f"Error: TOML parsing error: {e}")
-        return None
+	# Load config settings from txt file
+	def load(self):
+		try:
+			with open(self.config_path, "rb") as f:
+				data = tomlkit.load(f)
+			return(data)
+		except FileNotFoundError:
+			print(f"Error: File not found at '{self.config_path}'")
+			return None
+		except tomlkit.TOMLDecodeError as e:
+			print(f"Error: TOML parsing error: {e}")
+			return None
 
-config = load_config("config.toml")
+	# Save config settings to txt file
+	def save(self, config_item, v):
+		# Key names from the Camera/Display Options menus
+		# Strip down, remove spaces and special characters, and make lowercase
+		k=config_item.lower()
+		k=re.sub(r'[^a-zA-Z0-9]', '', k)
+		config=Config().load()
+		config[k]=str(v)
+		with open(self.config_path, 'w') as file:
+		    tomlkit.dump(config, file)
 
-white=config['whitebalance']
-
-print(white)
-
-"""
-def load_config(file_path):
-    try:
-        with open(file_path, "r") as f:
-            return tomli.load(f)
-    except FileNotFoundError:
-        print(f"Error: File not found at '{file_path}'")
-        return None
-    except tomli.TOMLDecodeError as e:
-        print(f"Error: TOML parsing error: {e}")
-        return None
-
-config = load_config(Path("config.toml"))
-
-if config:
-    print(f"WB: {config['whitebalance']}")
-else:
-    print("no config")
-
-"""
+Config().save('whitebalance', 'indoor')

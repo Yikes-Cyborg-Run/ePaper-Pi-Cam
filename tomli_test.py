50fc22b36f3070ff554e0deae597e38a983994ee
Yikes-Cyborg-Run <manager@theparsley.com>
2025-06-16 14:12:33 -0400

    toml edits

--- tomli_test.py
+++ tomli_test.py
@@ -1,9 +1,29 @@
 import tomli
+from pathlib import Path
 
-def load_config(file_path):
-    """Loads a TOML configuration file."""
+
+def load_config(config_path):
     try:
-        with open(file_path, "rb") as f:
+        with open(config_path, "rb") as f:
+            data = tomli.load(f)
+        return(data)
+    except FileNotFoundError:
+        print(f"Error: File not found at '{file_path}'")
+        return None
+    except tomli.TOMLDecodeError as e:
+        print(f"Error: TOML parsing error: {e}")
+        return None
+
+config = load_config("config.toml")
+
+white=config['whitebalance']
+
+print(white)
+
+"""
+def load_config(file_path):
+    try:
+        with open(file_path, "r") as f:
             return tomli.load(f)
     except FileNotFoundError:
         print(f"Error: File not found at '{file_path}'")
@@ -12,9 +32,11 @@ def load_config(file_path):
         print(f"Error: TOML parsing error: {e}")
         return None
 
-config = load_config(r"C:/Users/ckingsbury/OneDrive - City of Port Orange/Desktop/ePaperPiCam/ePaper-Pi-Cam/config.toml")
+config = load_config(Path("config.toml"))
 
 if config:
     print(f"WB: {config['whitebalance']}")
 else:
-    print("no config")    
+    print("no config")
+
+"""

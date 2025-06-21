a836bd081ae1979446a8dc6352b96368c646cd7b
Yikes-Cyborg-Run <40796089+Yikes-Cyborg-Run@users.noreply.github.com>
2025-06-20 20:16:26 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def main():
 			# Manually Scroll through photos
 			# Option to delete when photo button is pushed 
 			elif sel=='Manual Scroll':
-				if len(photo_list>0):
+				if len(photo_list)>0:
 					data=Action().manual_scroll(u, d, photo_list, data)
 					if p.is_pressed:
 						data=[0, 'Delete', False, num]

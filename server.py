8971ca8f95aecd0bedf43498e2e4e15f1fcad18e
Yikes-Cyborg-Run <40796089+Yikes-Cyborg-Run@users.noreply.github.com>
2025-08-23 07:43:04 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os, socket
 
+# Enter the IP address of your camera below
+IPAddr='xxx.xxx.xxx.xxx'
+
 app=Flask(__name__)
 PHOTO_FOLDER='/home/pi/ePaper-Pi-Cam/Photos'
 app.config['PHOTO_FOLDER']=PHOTO_FOLDER
@@ -23,7 +26,7 @@ def download_archive(filename):
     return send_from_directory(app.config['ARCHIVE_FOLDER'], filename, as_attachment=True)
 
 def serve(app):
-    app.run(host='xxx.xxx.x.xxx', port=5000, debug=True) # Run on all interfaces, default port
+    app.run(host=IPAddr, port=5000, debug=True) # Run on all interfaces, default port
 
 if __name__=='__main__':
     serve(app)
